17ea824c2d2d9d0a838f6db84525593755c5e423
Jesse Lentz <jlentzreceipts@gmail.com>
2012-08-10 20:42:08 -0400

    Initial commit

---
+++ Testimonials_files/Testimonials.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.1 local-build-20091106
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Testimonials_files/TestimonialsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');fixAllIEPNGs('Media/transparent.gif');IMpreload('Testimonials_files','shapeimage_2','0');IMpreload('Testimonials_files','shapeimage_3','0');IMpreload('Testimonials_files','shapeimage_4','0');IMpreload('Testimonials_files','shapeimage_5','0');performPostEffectsFixups()}
