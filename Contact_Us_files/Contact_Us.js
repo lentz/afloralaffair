be0b3702ac0c4491001ad206002ffeaa7c43dd45
Jesse Lentz <jlentzreceipts@gmail.com>
2012-08-10 23:00:28 -0400

    Removing project site files and adding readme

--- Contact_Us_files/Contact_Us.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.1 local-build-20091106
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Contact_Us_files/Contact_UsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');IMpreload('Contact_Us_files','shapeimage_2','0');IMpreload('Contact_Us_files','shapeimage_3','0');IMpreload('Contact_Us_files','shapeimage_4','0');IMpreload('Contact_Us_files','shapeimage_5','0');performPostEffectsFixups()}
